4ae090611a688d868c7c9884d0c5da4b35a66441
Dan Amador <danamadorpe@gmail.com>
2021-08-25 10:51:44 +0200

    Scenario Prepartations by Konrad

--- src/config.py
+++ src/config.py
@@ -19,6 +19,11 @@ class Levels:
 
 class Cars:
     ETK = "etk800"
+    VAN = "van"
+    ETKC = "etkc"
+    TRUCK = "semi"
+    OLD = "Serpent Beverly Sedan 1959"
+    DOPE = "2010 Dopel Imperium EcoTec"
 
 
 """
