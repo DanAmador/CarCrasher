9e0e3791b3a6235eb6b3e2d03297190b1821f101
KnrdRaue <45028195+KnrdRaue@users.noreply.github.com>
2021-08-27 13:14:26 +0200

    Add vehicles and camera positions for first scene

--- src/CustomScenarios/TestScenarios.py
+++ src/CustomScenarios/TestScenarios.py
@@ -46,26 +46,170 @@ class JsonLoaderScenarioTest(WithLidarView):
 
     def setup_scenario(self) -> SceneData:
         test_json = {
-            "level": "smallgrid",
+            "level": "Industrial",
             "cars": [
+                #{
+                #    "car_id": "test_car",
+                #    "position": [4.40, -355.40, 1.01, 0.0182628, -0.000119876, 0.00656274, 0.999812],
+                #    "model": "etk800",
+                #    "ai": "span",
+                #    "max_speed": 300,
+                #    "first_person": True
+                #},
                 {
-                    "car_id": "test_car",
-                    "position": [59.03, 86.14, 3.05, 0.0189849, -0.00283056, 0.147438, 0.988885],
+                    "car_id": "police_back_right",
+                    "position": [-383.54, -112.78, 42.34, 0.0142582, -0.0141653, 0.704651, 0.70927],
                     "model": "etk800",
                     "ai": "span",
-                    "max_speed": 300,
+                    "max_speed": 40,
+                    "first_person": True
+                },
+                {
+                    "car_id": "police_back_left",
+                    "position": [-383.56, -109.81, 42.34, 0.0142582, -0.0141653, 0.704651, 0.70927],
+                    "model": "etk800",
+                    "ai": "span",
+                    "max_speed": 40,
+                    "first_person": True
+                },
+                {
+                    "car_id": "police_front_right",
+                    "position": [-308.98, -114.53, 41.96, -0.000485358, 0.000511862, 0.725644, 0.68807],
+                    "model": "etkc",
+                    "ai": "span",
+                    "max_speed": 40,
+                    "first_person": True
+                },
+                {
+                    "car_id": "police_front_left",
+                    "position": [-308.78, -110.63, 41.75, 0.00781297, -0.00737831, 0.686554, 0.726999],
+                    "model": "etkc",
+                    "ai": "span",
+                    "max_speed": 40,
+                    "first_person": True
+                },
+                {
+                    "car_id": "oldtimner",
+                    "position": [-303.60, -113.25, 41.61, -0.000504249, 0.000493366, 0.699351, 0.714778],
+                    "model": "Serpent Beverly Sedan 1959",
+                    "ai": "span",
+                    "max_speed": 40,
+                    "first_person": True
+                },
+                {
+                    "car_id": "orange_van_front_left",
+                    "position": [-320.13, -110.92, 43.24, 0.0520454, -0.0464531, 0.664266, 0.744234],
+                    "model": "van",
+                    "ai": "span",
+                    "max_speed": 40,
+                    "first_person": True
+                },
+                {
+                    "car_id": "orange_van_front_right",
+                    "position": [-319.83, -114.49, 42.98, 0.0260095, -0.0259837, 0.706278, 0.706979],
+                    "model": "van",
+                    "ai": "span",
+                    "max_speed": 40,
+                    "first_person": True
+                },
+                {
+                    "car_id": "white_van",
+                    "position": [-330.48, -113.98, 42.77, 0.0259258, -0.0260442, 0.708237, 0.705017],
+                    "model": "van",
+                    "ai": "span",
+                    "max_speed": 40,
+                    "first_person": True
+                },
+                {
+                    "car_id": "murica_truck",
+                    "position": [-332.35, -109.85, 43.52, 0.088969, -0.0917006, 0.711833, 0.690629],
+                    "model": "semi",
+                    "ai": "span",
+                    "max_speed": 40,
+                    "first_person": True
+                },
+                {
+                    "car_id": "van_truck_left",
+                    "position": [-341.54, -109.74, 43.05, 0.00240331, -0.0024651, 0.716019, 0.698072],
+                    "model": "van",
+                    "ai": "span",
+                    "max_speed": 40,
+                    "first_person": True
+                },
+                {
+                    "car_id": "van_truck_right",
+                    "position": [-341.65, -113.45, 43.05, 0.0250068, -0.02549, 0.713384, 0.699863],
+                    "model": "van",
+                    "ai": "span",
+                    "max_speed": 40,
+                    "first_person": True
+                },
+                {
+                    "car_id": "orange_truck_back_left",
+                    "position": [-351.00, -109.75, 42.65, 0.0269897, -0.0279153, 0.718383, 0.694564],
+                    "model": "van",
+                    "ai": "span",
+                    "max_speed": 40,
+                    "first_person": True
+                },
+                {
+                    "car_id": "orange_truck_back_right",
+                    "position": [-350.75, -113.62, 42.57, 0.0295953, -0.0297306, 0.708095, 0.70487],
+                    "model": "van",
+                    "ai": "span",
+                    "max_speed": 40,
+                    "first_person": True
+                },
+                {
+                    "car_id": "first_google_car",
+                    "position": [-256.28, -106.18, 41.35, 0.0197539, -0.100565, 0.976082, 0.191731],
+                    "model": "2010 Dopel Imperium EcoTec",
+                    "ai": "disabled",
+                    "max_speed": 0,
+                    "first_person": True
+                },
+                {
+                    "car_id": "second_google_car",
+                    "position": [-213.34, -10.10, 40.60, 0.0345355, -0.0142065, 0.380164, 0.924165],
+                    "model": "2010 Dopel Imperium EcoTec",
+                    "ai": "disabled",
+                    "max_speed": 0,
                     "first_person": True
                 }
             ],
             "cameras": [
                 # {
-                #     "position": [49.60, 90.78, 6.16, 0.137144, -0.187947, 0.785636, 0.573275],
-                #     "fov": 50
+                #     "position": [13.78, -53.91, 8.90, 0.15007, 0.020832, -0.135909, 0.979068]
                 # },
-                {
-                    "position": [60.19, 78.05, 7.42, 0.375808, 0.0165959, -0.040877, 0.925647],
-                    "fov": 80
-                }
+                # {
+                #     "position": [-334.97, 290.03, 113.97, 0.126316, 0.142928, -0.735552, 0.650061]
+                # }
+
+                #Actual Cameras. just commented out for now
+                #Initial Shot1
+                # {
+                #     "position": [-238.73, -89.77, 66.70, -0.12936, -0.24496, 0.849666, -0.448697]
+                # }
+                #Initial Shot2... yes there are 2 of them, but anton s going to edit so whatevs...
+                # {
+                #     "position": [-241.75, -120.30, 55.37, 0.142871, 0.0873563, -0.514284, 0.841112]
+                # }
+                #second shot (with google car?)
+                # {
+                #     "position": [-258.25, -105.68, 42.58, 0.0113626, -0.0385524, 0.958431, 0.282479]
+                # }
+                #third shot
+                # {
+                #     "position": [-224.80, 44.95, 72.62, 0.046881, -0.210012, 0.953115, 0.212764]
+                # }
+                # last shot
+                # {
+                #     "position": [-334.89, 274.13, 55.78, 0.148702, -0.0178154, 0.117614, 0.981701]
+                # }
+                # fourth shot (with google car)
+                # {
+                #     "position": [-219.52, -8.03, 40.56, -0.0220088, 0.015967, 0.587006, 0.809126]
+                # }
             ]
         }
         # position 1 euler =  -33.249096, -30.807463,132.597778
