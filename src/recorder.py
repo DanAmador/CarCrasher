7d152ee5009a395befc9b9f98ca48988768958da
Dan Amador <danamadorpe@gmail.com>
2021-08-31 16:44:59 +0200

    Hotkey recording

--- src/recorder.py
+++ src/recorder.py
@@ -12,15 +12,12 @@ if __name__ == "__main__":
     bb = BeamBuilder(launch=True, steps_per_sec=framerate)
 
     scenario = cs.JsonLoaderScenarioTest(bb)
-    # bb.bmng.set_relative_camera(pos=(2,2,2))
 
-    # sequence = ImageSequence(data_path / "captures")
-    # scenario.initialize_scenario()
     manager = SequenceManager(bb, scenario)
     print("Press S key to record clip")
     while True:
         # time.sleep(3)
-        if scenario.should_record_predicate():
+        if scenario.should_record_predicate() or keyboard.is_pressed("s"):
             print("Starting recording")
             bb.bmng.set_steps_per_second(framerate * simulation_steps_per_frame)
             manager.capture_footage(simulation_steps_per_frame=simulation_steps_per_frame, framerate=framerate, duration=5
