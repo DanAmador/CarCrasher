574a86eb36a473c1c96b145d6a1ba569574ee3c8
Konrad Raue / Dan Amador <raue.konrad@t-online.de>
2021-09-20 15:46:02 +0200

    part config works and convoy is set up

--- src/CustomScenarios/ComplexScenarios.py
+++ src/CustomScenarios/ComplexScenarios.py
@@ -17,7 +17,6 @@ class FirstScenario(WithLidarView):
     @staticmethod
     def poly_script_from_points(poly, speed):
         old = 0
-        print(poly)
         for p in poly:
             print(p)
             val = p.get('t')
@@ -26,14 +25,22 @@ class FirstScenario(WithLidarView):
             old = old + val * speed
         return poly
 
+    @staticmethod
+    def apply_offset(poly, offset):
+        for p in poly:
+            val = p.get('t')
+            p.update({'t': val + offset})
+        return poly
+
     def setup_scenario(self) -> SceneData:
         scenario_path = us.json_path / "first_scenario"
-        sd, jay = SceneData.from_json_file(scenario_path / "middle_scene_description.json", self.bb)
+        sd, jay = SceneData.from_json_file(scenario_path / "final_scene_description.json", self.bb)
         lane_names = ["central", "left", "right"]
 
         lanes = {
             lane: FirstScenario.poly_script_from_points(json.loads(
-                (scenario_path / f"new_lane_{lane}.json").read_text()), 2)for lane in lane_names}
+                (scenario_path / f"final_lane_{lane}.json").read_text()), 2)for lane in lane_names}
+        self.bb.bmng.pause()
 
         # position 1 euler = -2.391787, 3.153309,52.855541
         #
@@ -46,10 +53,27 @@ class FirstScenario(WithLidarView):
         #                                   rgba_colors=[(lane_color, lane_color, lane_color,1) for p in points])
         #    self.bb.bmng.add_debug_polyline(points, rgba_color=(lane_color, lane_color, lane_color, 1), cling=True)
 
+        #this part works, but we need to wait until it is loaded in
+        for car in jay["cars"]:
+            if "parts" in car:
+                vehicle = sd.vehicles[car["car_id"]]
+                vehicle.set_part_config(car["parts"])
+
         for car in jay["cars"]:
             if "lane" in car:
                 lane = lanes.get(car["lane"], None)
+                if "offset" in car:
+                    temp = FirstScenario.apply_offset(lane, car["offset"])
+                    lane = temp
                 vehicle = sd.vehicles[car["car_id"]]
                 vehicle.ai_set_script(lane)
 
+            #if "ai" in car:
+            #    vehicle = sd.vehicles[car["car_id"]]
+            #    vehicle.ai_set_mode(car["ai"])
+            #    if car["ai"] == "chase":
+            #        target = car["target"]
+            #        vehicle.ai_set_target(target, "chase")
+            #        vehicle.ai_set_speed(1, "limit")
+        self.bb.bmng.resume()
         return sd
